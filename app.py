e0425104ed7ab0b877be00f83cd643be45e688a7
ali1234646 <hyderabadia053@gmail.com>
2025-11-28 20:50:53 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+print("Welcome to AsaanAI!")
+while True:
+    question = input("Ask anything: ")
+    if question.lower() in ["exit", "quit"]:
+        print("Goodbye!")
+        break
+    else:
+        print("Your AI will answer this in future updates!")
